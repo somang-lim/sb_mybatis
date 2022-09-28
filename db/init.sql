a8b07d18c7ff7b28bd288aebb553708ef03e0ff0
somang-lim <somangjuha@gmail.com>
2022-09-28 13:25:53 +0900

    회원 스키마 추가

--- db/init.sql
+++ db/init.sql
@@ -1,7 +1,9 @@
+# DB 생성
 DROP DATABASE IF EXISTS sb_mybatis;
 CREATE DATABASE sb_mybatis;
 USE sb_mybatis;
 
+# 게시물 테이블 생성
 CREATE TABLE article (
     id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     PRIMARY KEY(id),
@@ -11,6 +13,7 @@ CREATE TABLE article (
     content LONGTEXT NOT NULL
 );
 
+# 게시물 데이터
 INSERT INTO article
 SET createDate = NOW(),
 modifyDate = NOW(),
@@ -22,3 +25,35 @@ SET createDate = NOW(),
 modifyDate = NOW(),
 `subject` = '제목2',
 content = '내용2';
+
+# 회원 테이블 생성
+CREATE TABLE `member` (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id),
+    createDate DATETIME NOT NULL,
+    modifyDATE DATETIME NOT NULL,
+    username CHAR(100) NOT NULL UNIQUE,
+    `password` CHAR(100) NOT NULL,
+    `name` CHAR(100) NOT NULL,
+    email CHAR(100) NOT NULL
+);
+
+# 회원 데이터
+INSERT INTO `member`
+SET createDate = NOW(),
+modifyDate = NOW(),
+username = 'user1',
+`password` = '{noop}1234',
+`name` = '유저1',
+email = 'user1@test.com';
+
+INSERT INTO `member`
+SET createDate = NOW(),
+modifyDate = NOW(),
+username = 'user2',
+`password` = '{noop}1234',
+`name` = '유저2',
+email = 'user2@test.com';
+
+SELECT *
+FROM `member`;
