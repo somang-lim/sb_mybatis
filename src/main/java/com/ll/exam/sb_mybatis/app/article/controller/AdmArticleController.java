a29d59e3a68c8af264cc6a12670a3debff09bb46
somang-lim <somangjuha@gmail.com>
2022-10-04 13:12:55 +0900

    관리자 페이지 추가

---
+++ src/main/java/com/ll/exam/sb_mybatis/app/article/controller/AdmArticleController.java
@@ -0,0 +1,42 @@
+package com.ll.exam.sb_mybatis.app.article.controller;
+
+import com.ll.exam.sb_mybatis.app.article.dto.Article;
+import com.ll.exam.sb_mybatis.app.article.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/adm/article")
+@RequiredArgsConstructor
+@Slf4j
+public class AdmArticleController {
+    private final ArticleService articleService;
+
+    @GetMapping("/list")
+    public String showList(Model model) {
+        List<Article> articles = articleService.getForPrintArticles();
+
+        log.debug("articles : " + articles);
+
+        model.addAttribute("articles", articles);
+
+        return "adm/article/list";
+    }
+
+    @GetMapping("/{id}")
+    public String showDetail(@PathVariable Long id) {
+        return "adm/article/detail";
+    }
+
+    @GetMapping("/write")
+    public String showWrite() {
+        return "adm/article/write";
+    }
+}
